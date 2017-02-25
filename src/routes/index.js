67ab3e2d5db06728b92204b1260b5d6a365a92db
Nathaniel D Alcedo Jr <nathanielalcedojr@gmail.com>
2017-02-25 15:32:56 +0800

    sorted imports and added authentication middleware for entry route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
-import express from 'express';
 import authentication from './authentication';
-import signup from './signup';
 import entry from './entry';
+import express from 'express';
+import signup from './signup';
+import authenticateToken from '../middleware/authenticateToken';
 
 const router = express.Router();
 
 router.use('/authentication', authentication);
 router.use('/signup', signup);
-router.use('/entry', entry);
+router.use('/entry', authenticateToken, entry);
 
 export default router;
