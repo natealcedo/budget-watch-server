08b2c270e259e939b5d73008be05784bd836bd76
Nathaniel D Alcedo Jr <nathanielalcedojr@gmail.com>
2017-03-06 23:29:21 +0800

    indent

--- src/controllers/entry.js
+++ src/controllers/entry.js
@@ -1,82 +1,82 @@
-import db from '../models';
+import db from "../models";
 
 const entryController = {};
 
 entryController.post = function(req,res){
-    const { userId } = req.user;
-    const { category, description, amount, year, month, day } = req.body;
-    const entry = new db.Entry({
-        _user: userId,
-        amount,
-        category,
-        day,
-        description,
-        month,
-        year,
+  const { userId } = req.user;
+  const { category, description, amount, year, month, day } = req.body;
+  const entry = new db.Entry({
+    _user: userId,
+    amount,
+    category,
+    day,
+    description,
+    month,
+    year,
+  });
+  entry.save().then(newEntry => {
+    res.status(201).json({
+      sucess: true,
+      newEntry
     });
-    entry.save().then(newEntry => {
-        res.status(201).json({
-            sucess: true,
-            newEntry
-        });
-    }).catch(err => {
-        res.statu(500).json({
-            errors: err
-        });
+  }).catch(err => {
+    res.statu(500).json({
+      errors: err
     });
+  });
 };
 
 entryController.getAll = function(req,res){
-    const { userId } = req.user;
-    db.Entry.find({
-        _user: userId
-    }).select('description amount date category').then(entries => {
-        res.status(200).json({
-            sucess: true,
-            entries
-        });
-    }).catch(err => {
-        res.status(500).json({
-            errors: err
-        });
+  const { userId } = req.user;
+  db.Entry.find({
+    _user: userId
+  }).select("description amount date category").then(entries => {
+    res.status(200).json({
+      sucess: true,
+      entries
     });
+  }).catch(err => {
+    res.status(500).json({
+      errors: err
+    });
+  });
 };
 
 entryController.getByMonth = function(req,res){
-    const { userId } = req.user;
-    const { month, year } = req.body;
-    db.Entry.find({
-        _user: userId,
-        year: year,
-        month: month
-    }).select('description amount year month day category').then(entries=> {
-        res.status(200).json({
-            sucess: true,
-            entries
-        });
-    }).catch(err => {
-        res.status(500).json({
-            errors: err
-        });
+  const { userId } = req.user;
+  const { month, year } = req.body;
+  db.Entry.find({
+    _user: userId,
+    year: year,
+    month: month
+  }).select("description amount year month day category").then(entries=> {
+    res.status(200).json({
+      sucess: true,
+      entries
     });
+  }).catch(err => {
+    res.status(500).json({
+      errors: err
+    });
+  });
 };
 
 entryController.getByYear = function(req,res){
-    const { userId } = req.user;
-    const { year } = req.body;
-    db.Entry.find({
-        _user: userId,
-        year: year
-    }).select('description amount year month day category').then(entries=> {
-        res.status(200).json({
-            sucess: true,
-            entries
-        });
-    }).catch(err => {
-        res.status(500).json({
-            errors: err
-        });
+  const { userId } = req.user;
+  const { year } = req.body;
+  db.Entry.find({
+    _user: userId,
+    year: year
+  }).select("description amount year month day category").then(entries=> {
+    res.status(200).json({
+      sucess: true,
+      entries
     });
+  }).catch(err => {
+    res.status(500).json({
+      errors: err
+    });
+  });
 };
 
 export default entryController;
--- src/models/User.js
+++ src/models/User.js
@@ -1,28 +1,27 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose, { Schema } from "mongoose";
 
 mongoose.Promise = global.Promise;
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        minLength: [5, 'Username must be 5 characters or more'],
-        unique: true
-    },
-    password_digest: {
-        type: String,
-        required: true,
-        minLength: [8, 'Password must be 8 characters or more']
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    createdAt: { type: Date, default: Date.now },
+  username: {
+    type: String,
+    required: true,
+    minLength: [5, "Username must be 5 characters or more"],
+    unique: true
+  },
+  password_digest: {
+    type: String,
+    required: true,
+    minLength: [8, "Password must be 8 characters or more"]
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  createdAt: { type: Date, default: Date.now },
 });
 
-
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 export default User;
 
